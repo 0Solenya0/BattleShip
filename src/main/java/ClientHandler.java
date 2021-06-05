2066d9e24af51d64c4dfc8c4097869ac26375c29
Mani <manimanihajiggl@gmail.com>
2021-06-06 03:19:38 +0430

    init

---
+++ src/main/java/ClientHandler.java
@@ -0,0 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.Socket;
+
+public class ClientHandler {
+    private static final Logger logger = LogManager.getLogger(ClientHandler.class);
+
+    public ClientHandler(Socket socket) {
+        socket.getInputStream()
+    }
+}
